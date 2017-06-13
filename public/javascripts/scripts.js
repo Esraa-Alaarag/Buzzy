03c326c91d38ae295398fbd11c1a8282ae73c021
Esraa Alaarag <m_israa1981@hotmail.com>
2017-06-13 16:42:15 -0400

    some improvemnt in js

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,41 +1,45 @@
-function getUserAddress(e){
-	console.log("I was clicked")
-}
 
-$('#getAddress').on('click', function(){
-    addEventListener('click', getUserAddress)
-	getLocation();
-});
 
-$('#enteredAddress').keydown( function( event ) {
-    if ( event.which === 13 ) {
-    	alert(event.currentTarget.value)
-        event.preventDefault();
-        return false;
-    }
-});
 
-function getLocation() {
-    if (navigator.geolocation) {
+$( "#getAddress" ).click(function() {
+ if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else { 
         console.log("Geolocation is not supported by this browser.");
     }
-}
+});
+
 
 function showPosition(position) {
+	var geocoder = new google.maps.Geocoder;
    console.log("Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude);
-   getevents();
+   var latlng = {lat: parseFloat(position.coords.latitude), lng: parseFloat(position.coords.longitude)};
+   geocoder.geocode({'location': latlng}, function(results, status) {
+          if (status === 'OK') {
+       			console.log(results)
+              $('#enteredAddress').val(results[0].formatted_address)
+             //document.getElementById('enteredAddress').value = results[1].formatted_address;
+            } else {
+              window.alert('No results found');
+            }
+        });
 }
 function getevents(){
 $.ajax({url: "https://api.cityofnewyork.us/calendar/v1/search.htm?app_id=50d4593b&app_key=53d1241d31d9ddf5a0f1ed73708f05f9&categories=Kids+and+family&zip=10001",
-     json: "callback",
-      dataType: "json",
+     jsonp: "callback",
+      dataType: "jsonp",
        success: function(response) {
      console.log(response);
   }
 })
       }
+
+var queryString = $('#mainForm').serialize();
+console.log(queryString)
+
+
+
+
 
 
