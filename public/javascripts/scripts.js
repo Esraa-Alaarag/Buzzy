777a110ec128fb8883ec66e7658c0d373719f21e
Esraa Alaarag <m_israa1981@hotmail.com>
2017-06-15 16:10:15 -0400

    done with the map

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,9 +1,14 @@
+var isitclicked = false;
+
+function mapInitialize() {
+    myMap(40.7400245, -73.9897259, 10);
+}
 
 $("#getAddress").click(getTheAddress)
 
-function getTheAddress(){
+function getTheAddress() {
     $('#enteredAddress').val(" ")
-   $("#getAddress").replaceWith("<img class='loading' src='/images/loading.gif' alt='loading' />").fadeIn();  
+    $("#getAddress").replaceWith("<img class='loading' src='/images/loading.gif' alt='loading' />").fadeIn();
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
@@ -12,34 +17,40 @@ function getTheAddress(){
 };
 
 $("#getAddress").click(function () {
-   document.getElementById("mainForm").reset();
+    document.getElementById("mainForm").reset();
 });
 
 
 
 
-var zip=" ";
+var latlng = {};
+
 function showPosition(position) {
+    isitclicked = true;
     var geocoder = new google.maps.Geocoder;
     console.log("Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude);
-    var latlng = {
+        "Longitude: " + position.coords.longitude);
+    myMap(position.coords.latitude, position.coords.longitude, 15)
+    latlng = {
         lat: parseFloat(position.coords.latitude),
         lng: parseFloat(position.coords.longitude)
     };
     geocoder.geocode({
         'location': latlng
     }, function (results, status) {
+        var humanaddress;
         if (status === 'OK') {
-            console.log(results)
-            zip=parseInt(results[1].address_components[6].long_name , 10)
-            console.log(zip)
+            //console.log(results)
             $(".loading").replaceWith("<a class='waves-effect waves-light btn-large' id='getAddress'> or Click here to get your location</a>").fadeIn();
-            $('#enteredAddress').val(results[0].formatted_address)
+            var humanaddress;
+            humanaddress = results[0].formatted_address;
+
+            console.log("in" + humanaddress);
+            $('#enteredAddress').val(humanaddress)
             $("#getAddress").click(getTheAddress)
-            //document.getElementById('enteredAddress').value = results[1].formatted_address;
         } else {
-            window.alert('No results found');
+            console.log("not exist" + results);
+
         }
     });
 }
@@ -56,32 +67,34 @@ $('#mainForm').on('submit', function () {
     $(':input').each(function () {
         userPref.push($(this).val());
     });
-    console.log(userPref);
-    var address= userPref[0];
-    var startDate=userPref[1];
-    var endDate=userPref[2];
-    startDate=FormatDate(startDate)
-    endDate=FormatDate(endDate)
+    //console.log(userPref);
+    var address = userPref[0];
+    var startDate = userPref[1];
+    //console.log(startDate);
+    var endDate = userPref[2];
+    startDate ? FormatDate(startDate) : ""
+    endDate ? FormatDate(endDate) : ""
+    var searchdate ;
+    startDate? searchdate = startDate + "-" + endDate : searchdate=" "
+    //console.log(searchdate);
     // var startDate="06%2F14%2F2017+12%3A00+am";
     // var endDate="06%2F16%2F2017+12%3A00+am";
-    var Boroughs=$("#borough").val().join('+');
-    var categories=$("#Category").val().join('+');
-    
-    console.log(zip , startDate , endDate , Boroughs, categories )
+    var categories = $("#Category").val().join('+');
+
+    //console.log(searchdate , Boroughs, categories )
     //for()
-    getevents()
-    
+
+    getevents(searchdate, categories)
+
 });
 
-function FormatDate(date){
-    date=date.split("-")
+function FormatDate(date) {
+    date = date.split("-")
     console.log(date);
-    var Day=date[2];
-    var Month=date[1];
-    var Year=date[0];
-    datestring=Month+"%2F"+Day+"%2F"+Year+"+"+"12"+"%3A"+"00"+"+"+"am";
-    console.log(datestring);
-    console.log("06%2F14%2F2017+12%3A00+am");
+    var Day = date[2];
+    var Month = date[1];
+    var Year = date[0];
+    datestring = Year + Month + Day + "00";
     return datestring;
 }
 
@@ -114,57 +127,87 @@ function FormatDate(date){
 //         }
 //     })
 
-    
+
 // }
 
-function getevents (){
+function getevents(date, Category) {
+    var location
+    latlng.lat?location = `${latlng.lat},${latlng.lng}`: location= "new york";
+     
     var oArgs = {
 
-      app_key: "c8k7HD8DxtLXZswH",
+        app_key: "c8k7HD8DxtLXZswH",
 
-      q: "kids",
+        location: location || "new york",
 
-      where: "New York", 
+        within: 25,
 
-      "date": "2017061000-2017062000",
+        date:  date || " ",
 
-      page_size: 20,
+        page_size: 10,
 
-      sort_order: "popularity",
+        sort_order: "date",
 
-      id: "family_fun_kids"
+        category: Category || " ",
+    };
+    console.log(oArgs);
+    // EVDB.API.call("/categories/list", oArgs, function(oData) {
+    EVDB.API.call("/events/search", oArgs, function (oData) {
 
-   };
-   // EVDB.API.call("/categories/list", oArgs, function(oData) {
-   EVDB.API.call("/events/search", oArgs, function(oData) {
+        console.log(oData);
+        var events = " ";
 
-      console.log(oData);
-      console.log(oData.events.event["0"].title); 
-      var events = " "
-            for(var i=0;i<10;i++){
-            events +=`<div class="section white">
+
+        for (var i = 0; i < 10; i++) {
+
+
+            events += `<div class="section white">
             <p>Event's name:${oData.events.event[i].title}</p>
-            <img class='eventpic' src=${oData.events.event[i].image.medium.url} alt='event picture' />
-            <p>Event website: <a href=${oData.events.event[i].url}>Click here</a></p>`}
-            document.getElementById("results").innerHTML=events;
+            <p>Event website: <a target="_blank" href=${oData.events.event[i].url}>Click here</a></p>
+            `
+        }
+
+        document.getElementById("results").innerHTML = events;
 
     });
 
 }
+// edpborder500 instead of medium
 
 
-   
-// <p>discription: ${description}</p>
-//             <p>Date: ${date}</p>
-//             <p>location lat: ${response.items[i].geometry[0].lat} lng ${response.items[i].geometry["0"].lng} </p>
-//             <p>Location: ${response.items[i].address}</p>
-//             <p>Time: ${response.items[i].timePart}</p>
-//             <p>Event Website: <a href=${response.items[i].permalink}>${response.items[i].permalink}</a></p>
-//             </div>
+function myMap(latitude, longitude, zoom) {
+    console.log(latitude, longitude, zoom);
+    var map = new google.maps.Map(document.getElementById('googleMap'), {
+        zoom: zoom,
+        center: {
+            lat: latitude,
+            lng: longitude
+        }
+    });
+    var geocoder = new google.maps.Geocoder;
+    var infowindow = new google.maps.InfoWindow;
 
+    var latlng = {
+        lat: latitude,
+        lng: longitude
+    }
+    if (isitclicked) {
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            //draggable: true,
+            animation: google.maps.Animation.DROP,
+        });
+        marker.addListener('click', toggleBounce)
+    }
 
+    function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    }
 
-
-
-
+};
 
